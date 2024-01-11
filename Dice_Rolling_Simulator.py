deacd32b9b93182694389dcb17b0e1ef83a4a54d
Aman Chaudhary <77241279+amancse2020@users.noreply.github.com>
2024-01-11 17:39:23 +0530

    Add files via upload

---
+++ Dice_Rolling_Simulator.py
@@ -0,0 +1,45 @@
+import random
+x='y'
+while x == "y":
+    x=random.randint(1,6)
+    if x == 1:
+        print("------")
+        print("      ")
+        print("  0   ")
+        print("      ")
+        print("------")
+    if x == 2:
+        print("------")
+        print("      ")
+        print("  0 0 ")
+        print("      ")
+        print("------")
+    if x == 3:
+        print("------")
+        print("0     ")
+        print("   0  ")
+        print("     0")
+        print("------")
+    if x == 4:
+        print("------")
+        print("  0 0 ")
+        print("      ")
+        print("  0 0 ")
+        print("------")
+    if x == 5:
+        print("------")
+        print("  0 0 ")
+        print("   0  ")
+        print("  0 0 ")
+        print("------")
+    if x == 6:
+        print("------")
+        print("  0 0 ")
+        print("  0 0 ")
+        print("  0 0 ")
+        print("------")
+    x = input("press y to roll again and n to exit:")
+print("\n")
+
+
+
